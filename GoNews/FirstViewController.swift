f70582b647b2bb6b0a16cf5a8048168d51bba040
Nidhi <guest1@anvis-macbook-pro.local>
2019-02-02 16:45:45 -0800

    Initial Commit

---
+++ GoNews/FirstViewController.swift
@@ -0,0 +1,20 @@
+//
+//  FirstViewController.swift
+//  GoNews
+//
+//  Created by Guest1 on 2/2/19.
+//  Copyright © 2019 Nidhi. All rights reserved.
+//
+
+import UIKit
+
+class FirstViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
